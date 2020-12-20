efde2942b8ea78dee5a6b6bc72ee942846342458
Ipsal <ipsalmuzakkir@gmail.com>
2020-12-21 01:42:40 +0700

    Set Up service media & setup sequelize

---
+++ service-media/routes/media.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
+
+/* Tambah models */
+const { Media } = require('../models');
+    
+
+/* GET Image */
+
+router.get('/', async (req, res) => {
+    const media = await Media.findAll();
+    return res.json({
+        status : 'success',
+        data : media
+    });
+});
+
+/* Post image */
+router.post('/', (req, res) => { 
+    const image = req.body.image;
+    
+    /* validasi tipe gambar base64 */
+    if (!isBase64(image, { mimeRequired: true})) {
+            return res.status(400).json({ status  :'error', message :'invalid based64' });
+        }
+    
+    base64Img.img(image, './public/images', Date.now(), async (err, filepath) => {
+        if (err) {
+            return res.status(400).json({ status: 'error',message : res.message });
+        }
+
+            /*  file name image di split bentuk array*/
+            // const filename = filepath.split('\\').pop().split('/').pop();
+    const filename = filepath.split("\\").pop().split("/").pop();
+
+            /* menyimpan file image */
+    const media = await Media.create({ image: `image/${filename}` });
+
+    return res.json({
+        status: 'success',
+        data: {
+        id : media.id,
+        image :`${req.get('host')}/images/${filename}`
+        }
+                    //message : 'oke berhasil'
+    });
+
+    })
+});
+
+module.exports = router;
+
+
+
+
+
