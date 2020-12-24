0e4e8f43eb3ad59ac30abfd0bba3583da442e0ad
Ipsal <ipsalmuzakkir@gmail.com>
2020-12-25 05:31:55 +0700

    13. Service Media - Membuat API Get User By Id

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -7,7 +7,7 @@ const usersHandler = require ('./handler/users');
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
 router.put('/:id', usersHandler.update);
-// router.get('/:id', usersHandler.getUser);
+router.get('/:id', usersHandler.getUser);
 // router.get('/', usersHandler.getUsers);
 // router.post('/logout', usersHandler.logout);
 
