899780bc327feb0a80b4aea4b83919e02518a146
Ipsal <ipsalmuzakkir@gmail.com>
2020-12-25 04:44:09 +0700

    8. Service User - Membuat Model User

--- service-user/seeders/20201224212933-user-seeders.js
+++ service-user/seeders/20201224212933-user-seeders.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); //pangggil bcrypt untuk ngehes password
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
