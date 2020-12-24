5a4bfee59757bbf5b48625cef7860f1dfe1b1833
Ipsal <ipsalmuzakkir@gmail.com>
2020-12-25 05:11:22 +0700

    10. Service User - Membuat API Register.mp4

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const usersHandler = require ('./handler/users');
+
+
+router.post('/register', usersHandler.register);
+// router.post('/login', usersHandler.login);
+// router.put('/:id', usersHandler.update);
+// router.get('/:id', usersHandler.getUser);
+// router.get('/', usersHandler.getUsers);
+// router.post('/logout', usersHandler.logout);
+
+
 
 module.exports = router;
